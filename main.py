e0510093e887b8f7298ca4908c8c5b76025db565
Swapnaneel <swapnaneelsarkhel@gmail.com>
2026-04-19 21:41:42 +0200

    Add tasks system (add-task, list-tasks)

--- main.py
+++ main.py
@@ -36,6 +36,46 @@ def list_notes():
     for note in notes:
         print(f"{note['id']}: {note['content']}")
 
+
+
+TASKS_FILE = "tasks.json"
+
+def load_tasks():
+    try:
+        with open(TASKS_FILE, "r") as f:
+            return json.load(f)
+    except:
+        return []
+
+def save_tasks(tasks):
+    with open(TASKS_FILE, "w") as f:
+        json.dump(tasks, f, indent=2)
+
+def add_task(content):
+    tasks = load_tasks()
+    task = {
+        "id": len(tasks) + 1,
+        "content": content,
+        "done": False,
+        "created_at": str(datetime.now())
+    }
+    tasks.append(task)
+    save_tasks(tasks)
+    print("Task added.")
+
+def list_tasks():
+    tasks = load_tasks()
+
+    if not tasks:
+        print("No tasks found.")
+        return
+
+    for task in tasks:
+        status = "✔" if task["done"] else "✘"
+        print(f"{task['id']}: [{status}] {task['content']}")
+
+
+
 # CLI handling
 if len(sys.argv) < 2:
     print("Usage: python main.py add-note \"your note\"")
@@ -50,6 +90,12 @@ if command == "add-note":
 elif command == "list-notes":
     list_notes()
 
+elif command == "add-task":
+    content = " ".join(sys.argv[2:])
+    add_task(content)
+
+elif command == "list-tasks":
+    list_tasks()
+
 else:
     print("Unknown command")
-
